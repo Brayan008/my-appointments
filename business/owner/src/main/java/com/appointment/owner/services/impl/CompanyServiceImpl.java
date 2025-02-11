2feed16c40f7ea3f34c6eef04d85995693792fcc
ArmandoSJ <just-jauregui@hotmail.com>
2025-02-11 14:27:49 -0600

    Updated services

--- business/owner/src/main/java/com/appointment/owner/services/impl/CompanyServiceImpl.java
+++ business/owner/src/main/java/com/appointment/owner/services/impl/CompanyServiceImpl.java
@@ -1,10 +1,15 @@
 package com.appointment.owner.services.impl;
 
+import com.appointment.commons.enums.Status;
+import com.appointment.commons.exceptions.ObjectNotFoundException;
+import com.appointment.owner.entities.CompanyEntity;
 import com.appointment.owner.repositories.CompanyRepository;
 import com.appointment.owner.services.CompanyService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -12,4 +17,52 @@ import org.springframework.stereotype.Service;
 public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository companyRepository;
+
+    @Override
+    public List<CompanyEntity> getCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public CompanyEntity getCompanyById(Long companyId) {
+        return companyRepository.findById(companyId)
+            .orElseThrow(() -> new ObjectNotFoundException("Company not found with id " + companyId));
+    }
+
+    @Override
+    public CompanyEntity createCompany(CompanyEntity company) {
+        return companyRepository.save(company);
+    }
+
+    @Override
+    public CompanyEntity updateCompany(CompanyEntity company, Long companyId) {
+        CompanyEntity currentCompany = this.getCompanyById(companyId);
+        currentCompany.setName(company.getName());
+        currentCompany.setLogo(company.getLogo());
+        currentCompany.setPhoneNumber(company.getPhoneNumber());
+        currentCompany.setInstagramUrl(company.getInstagramUrl());
+        currentCompany.setFacebookUrl(company.getFacebookUrl());
+        currentCompany.setMembershipId(company.getMembershipId());
+        currentCompany.setStatusId(company.getStatusId());
+        return this.createCompany(currentCompany);
+    }
+
+    @Override
+    public CompanyEntity disableById(Long companyId) {
+        CompanyEntity currentCompany = this.getCompanyById(companyId);
+        currentCompany.setStatusId(Status.DISABLED.getCode());
+        return this.createCompany(currentCompany);
+    }
+
+    @Override
+    public CompanyEntity enableById(Long companyId) {
+        CompanyEntity currentCompany = this.getCompanyById(companyId);
+        currentCompany.setStatusId(Status.ENABLED.getCode());
+        return this.createCompany(currentCompany);
+    }
+
+    @Override
+    public List<CompanyEntity> findByStatusId(Long statusId) {
+        return companyRepository.findByStatusId(statusId);
+    }
 }
