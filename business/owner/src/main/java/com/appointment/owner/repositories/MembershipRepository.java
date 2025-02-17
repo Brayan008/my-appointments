ececb49a6ac0d64554417aeecc85c540bf04f831
ArmandoSJ <just-jauregui@hotmail.com>
2025-02-17 13:34:34 -0600

    Fix: Added status_id EmployeeEntity

--- business/owner/src/main/java/com/appointment/owner/repositories/MembershipRepository.java
+++ business/owner/src/main/java/com/appointment/owner/repositories/MembershipRepository.java
@@ -3,5 +3,8 @@ package com.appointment.owner.repositories;
 import com.appointment.owner.entities.MembershipEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MembershipRepository extends JpaRepository<MembershipEntity, Long> {
+    List<MembershipEntity> findByStatusId(Long statusId);
 }
