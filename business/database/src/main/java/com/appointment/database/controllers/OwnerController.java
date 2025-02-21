27be48e5808c7a32c1a6081af05c2af7c403d922
ArmandoSJ <just-jauregui@hotmail.com>
2025-02-21 00:12:07 -0600

    Added initial services in database

--- business/database/src/main/java/com/appointment/database/controllers/OwnerController.java
+++ business/database/src/main/java/com/appointment/database/controllers/OwnerController.java
@@ -1,21 +1,61 @@
 package com.appointment.database.controllers;
 
+import com.appointment.database.entities.OwnerEntity;
 import com.appointment.database.services.OwnerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
+@Tag(name = "owner resources", description = "This APi serve all functionality for management owners")
+@Slf4j
+@AllArgsConstructor
 @RestController
-@RequestMapping("/owner")
+@RequestMapping("/owners")
 public class OwnerController {
+    private final Environment env;
 
-    @Autowired
-    private OwnerService ownerService;
+    private final OwnerService ownerService;
 
-    @Autowired
-    private Environment env;
+    @Operation(summary = "get owners")
+    @GetMapping()
+    public ResponseEntity<?> owners(){
+        log.info("Get: owners ");
+        return ResponseEntity.ok(this.ownerService.getOwners());
+    }
+
+    @Operation(summary = "get a owner given a owner id")
+    @GetMapping("/{ownerId}")
+    public ResponseEntity<?> ownerById(@PathVariable(name = "ownerId") Long ownerId){
+        log.info("Get: owner {}", ownerId);
+        return ResponseEntity.ok(this.ownerService.getOwnerById(ownerId));
+    }
+
+    @Operation(summary = "create a owner")
+    @PostMapping()
+    public ResponseEntity<?> createOwner(@RequestBody OwnerEntity owner, HttpServletRequest request){
+        log.info("create: owner {}", owner.getUserId());
+        OwnerEntity newOwner = this.ownerService.createOwner(owner);
+
+        String baseUrl = request.getRequestURI();
+        URI newLocation = URI.create(baseUrl + "/"+ newOwner.getOwnerId());
+
+        return ResponseEntity.created(newLocation).body(newOwner);
+    }
+
+    @Operation(summary = "update a owner by owner id")
+    @PutMapping("/{ownerId}")
+    public ResponseEntity<?> updateOwner(@PathVariable(name = "ownerId") Long ownerId,
+                                         @RequestBody OwnerEntity owner){
+        log.info("updating: owner {}", ownerId);
+        return ResponseEntity.ok(this.ownerService.updateOwner(owner, ownerId));
+    }
 
     @GetMapping("/check")
     public String check(){
--- business/database/src/main/java/com/appointment/database/services/impl/OwnerServiceImpl.java
+++ business/database/src/main/java/com/appointment/database/services/impl/OwnerServiceImpl.java
@@ -1,10 +1,15 @@
 package com.appointment.database.services.impl;
 
+import com.appointment.commons.exceptions.ObjectNotFoundException;
+import com.appointment.database.entities.OwnerEntity;
 import com.appointment.database.repositories.OwnerRepository;
 import com.appointment.database.services.OwnerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -13,4 +18,29 @@ public class OwnerServiceImpl implements OwnerService {
 
     private final OwnerRepository ownerRepository;
 
+    @Override
+    public List<OwnerEntity> getOwners() {
+        return ownerRepository.findAll();
+    }
+
+    @Override
+    public OwnerEntity getOwnerById(Long ownerId) {
+        return ownerRepository.findById(ownerId)
+            .orElseThrow(() -> new ObjectNotFoundException(HttpStatus.NOT_FOUND.value(),
+                "owner not found with id " + ownerId, HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public OwnerEntity createOwner(OwnerEntity owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public OwnerEntity updateOwner(OwnerEntity owner, Long ownerId) {
+        OwnerEntity currentOwner = this.getOwnerById(ownerId);
+        currentOwner.setUserId(owner.getUserId());
+        currentOwner.setCompanyId(owner.getCompanyId());
+        currentOwner.setOwnerHierarchy(owner.getOwnerHierarchy());
+        return this.createOwner(currentOwner);
+    }
 }
