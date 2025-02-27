df84858a265afd78be5a8c5db68737393615b05e
Brayan Mares <67080870+Brayan008@users.noreply.github.com>
2025-02-27 10:37:31 -0600

    Fix swagger database and add findStoresBySearchText

--- business/database/src/main/java/com/appointment/database/repositories/StoreRepository.java
+++ business/database/src/main/java/com/appointment/database/repositories/StoreRepository.java
@@ -2,9 +2,15 @@ package com.appointment.database.repositories;
 
 import com.appointment.database.entities.StoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
     List<StoreEntity> findByStatusId(Long statusId);
+
+    @Query(value = "SELECT * FROM stores WHERE name ILIKE %:searchText% OR address ILIKE %:searchText% OR coordinates ILIKE %:searchText%",
+        nativeQuery = true)
+    List<StoreEntity> searchStores(@Param("searchText") String searchText);
 }
