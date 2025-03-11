52556575ba3be6683c05ff01b521b26aa9220769
Brayan Mares <67080870+Brayan008@users.noreply.github.com>
2025-03-11 08:19:24 -0600

    Fix scan packages to can work components from commons

--- business/owner/src/main/java/com/appointment/owner/OwnerApplication.java
+++ business/owner/src/main/java/com/appointment/owner/OwnerApplication.java
@@ -3,7 +3,7 @@ package com.appointment.owner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.appointment.*"})
 public class OwnerApplication {
 
 	public static void main(String[] args) {
