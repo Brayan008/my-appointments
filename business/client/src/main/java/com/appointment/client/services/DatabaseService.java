e761beff23d052c2f41bdcf43a6d63b964d70ba6
Brayan Mares <67080870+Brayan008@users.noreply.github.com>
2025-03-31 08:04:11 -0600

    add rate appointment in client microservices

--- business/client/src/main/java/com/appointment/client/services/DatabaseService.java
+++ business/client/src/main/java/com/appointment/client/services/DatabaseService.java
@@ -4,6 +4,7 @@ import com.appointment.client.dtos.PageableClientAppointmentResponse;
 import com.appointment.client.dtos.StoreResponse;
 import com.appointment.commons.dtos.GenericResponse;
 import com.appointment.commons.dtos.request.ClientDBAppointmentRequest;
+import com.appointment.commons.dtos.request.RateAppointmentRequest;
 import com.appointment.commons.dtos.response.ClientDBAppointmentResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,5 +16,7 @@ public interface DatabaseService {
     Mono<GenericResponse> deleteFavoriteStore(Long userFavoriteStoreId);
     Flux<StoreResponse> findStoresBySearchText(String searchText);
     Mono<PageableClientAppointmentResponse> findClientAppointments(String email, int page, int size);
+    Mono<GenericResponse> addRateAppointment(Long idClientAppointment, RateAppointmentRequest rateAppointmentRequest);
+    Mono<GenericResponse> updateRateAppointment(Long idRateAppointment, RateAppointmentRequest rateAppointmentRequest);
 }
 
